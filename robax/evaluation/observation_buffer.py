53f7769e6d59eb0f2c4af59571d78945449ecc00
Michael Lutz <michaeljeffreylutz@gmail.com>
2025-01-15 06:57:02 +0000

    feat: added to sim evaluation

--- robax/evaluation/observation_buffer.py
+++ robax/evaluation/observation_buffer.py
@@ -1,26 +1,33 @@
 """Basic FIFO buffer for storing observations during evaluation."""
 
 from collections import deque
-from typing import Deque, Dict
+from typing import Deque, Dict, List
 
 import jax.numpy as jnp
 
+from robax.utils.observation import Observation, observation_from_dict
 
-class BatchedObservationBuffer:
+
+class ObservationBuffer:
     """Basic FIFO buffer that returns JAX array with observation history.
 
-    TODO: Make it use delta timesteps as well...
+    TODO: For environments with variance in timestamps / observation frequency, use timestamp logic.
     """
 
-    def __init__(self, observation_sizes: Dict[str, int]):
+    def __init__(self, delta_timestamps: Dict[str, List[float]], horizon_dim: int = 1):
         """Initialize the observation buffer.
 
         Args:
-            observation_sizes: A dictionary of observation sizes.
+            delta_timestamps: A dictionary of observation sizes.
+            horizon_dim: The observation or action horizon/history dimension. Should be consistent.
         """
-        self.buffers: Dict[str, Deque[jnp.ndarray]] = {
-            key: deque(maxlen=size) for key, size in observation_sizes.items()
+        self.observation_sizes = {
+            key: len(timestamps) for key, timestamps in delta_timestamps.items()
         }
+        self.buffers: Dict[str, Deque[jnp.ndarray]] = {
+            key: deque(maxlen=self.observation_sizes[key]) for key in self.observation_sizes
+        }
+        self.horizon_dim = horizon_dim
 
     def add(self, observation: Dict[str, jnp.ndarray]) -> None:
         """Add an observation to the buffer. Note that every observation should have a batch dim.
@@ -31,10 +38,18 @@ class BatchedObservationBuffer:
         for key, value in observation.items():
             self.buffers[key].append(value)
 
-    def get_observation(self) -> Dict[str, jnp.ndarray]:
-        """Get the observation history as a JAX array.
+    def get_observation(self) -> Observation:
+        """Get the observation history as a JAX array, padded with the oldest data if necessary.
 
         Returns:
-            A dictionary of observation history: key -> (batch_size, history_length, ...)
+            A dictionary of observation history: key -> (batch_size, horizon_length, ...)
         """
-        return {key: jnp.stack(list(self.buffers[key]), axis=1) for key in self.buffers}
+        padded_buffers = {}
+        for key, buffer in self.buffers.items():
+            buffer_list = list(buffer)
+            if len(buffer_list) < self.observation_sizes[key]:
+                repeat_count = self.observation_sizes[key] - len(buffer_list)
+                buffer_list = [buffer_list[0]] * repeat_count + buffer_list
+            padded_buffers[key] = jnp.stack(buffer_list, axis=self.horizon_dim)
+
+        return observation_from_dict(padded_buffers)
--- robax/evaluation/runner.py
+++ robax/evaluation/runner.py
@@ -1,22 +1,65 @@
 """Runs the evaluation loop."""
 
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 
 import gymnasium as gym
 import jax
 import jax.numpy as jnp
 
-from robax.evaluation.observation_buffer import BatchedObservationBuffer
+from robax.evaluation.observation_buffer import ObservationBuffer
 
 
 def batch_rollout(
+    prng: jax.Array,
+    params: Any,
     model: Any,
     env: gym.vector.VectorEnv,
-) -> None:
-    """Runs the evaluation loop."""
+    delta_timestamps: Dict[str, List[float]],
+    episode_length: int,
+    action_shape_to_generate: Tuple[int, ...],
+    **generate_action_kwargs: Any,
+) -> int:
+    """Runs the evaluation loop and returns the number of successful episodes.
 
-    observation_buffer = BatchedObservationBuffer(
-        env.observation_space,
-        env.action_space,
-        history_length=10,
+    Args:
+        prng: [B] PRNG key
+        params: model parameters
+        model: model
+        env: environment
+        delta_timestamps: delta timestamps
+        episode_length: episode length
+        action_shape_to_generate: action shape to generate
+        **generate_action_kwargs: additional kwargs for generate_action
+
+    Returns:
+        number of successful episodes
+    """
+
+    @jax.jit
+    def jitted_generate_action(prng, params, observation):
+        return model.apply(
+            prng=prng,
+            params=params,
+            observation=observation,
+            method="generate_action",
+            action_shape_to_generate=action_shape_to_generate,
+            **generate_action_kwargs,
+        )
+
+    observation_buffer = ObservationBuffer(
+        delta_timestamps=delta_timestamps,
+        horizon_dim=1,
     )
+    observations, infos = env.reset()
+    observation_buffer.add(observations)
+    success_count = 0
+    for _ in range(episode_length):
+        observation_batch = observation_buffer.get_observation()
+
+        action = jitted_generate_action(prng, params, observation_batch)
+
+        observations, _, terminateds, _, _ = env.step(action)
+        observation_buffer.add(observations)
+        success_count += int(jnp.sum(terminateds))
+
+    return success_count
