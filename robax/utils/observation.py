a0ab08c414f5d64bb0ed84d164c82c53cebb0ead
Michael Lutz <michaeljeffreylutz@gmail.com>
2025-01-15 06:57:21 +0000

    chore: added helpers for observation type

--- robax/utils/observation.py
+++ robax/utils/observation.py
@@ -1,6 +1,6 @@
 """Observation TypedDict"""
 
-from typing import TypedDict
+from typing import Dict, TypedDict
 
 import jax
 
@@ -8,11 +8,23 @@ import jax
 class Observation(TypedDict):
     """Observation TypedDict"""
 
-    images: jax.Array
+    images: jax.Array | None
     """Images (num_images, height, width, 3)"""
-    text: jax.Array
+    text: jax.Array | None
     """Text (num_text_tokens)"""
-    proprio: jax.Array
+    proprio: jax.Array | None
     """Proprioceptive features (proprio_history, num_proprio_features)"""
-    action: jax.Array
+    action: jax.Array | None
     """Action history and/or noisy actions (action_history, num_actions)"""
+
+
+def observation_from_dict(
+    observation_dict: Dict[str, jax.Array | None],
+) -> Observation:
+    """Create an observation"""
+    return Observation(
+        images=observation_dict.get("images", None),
+        text=observation_dict.get("text", None),
+        proprio=observation_dict.get("proprio", None),
+        action=observation_dict.get("action", None),
+    )
